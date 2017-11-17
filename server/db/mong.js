8de82cd35b4a7463f99d9c38b773d51865e3fd19
Geo <gaa39@mail.aub.edu>
2017-11-17 22:55:41 +0200

    changed mong to mongoose

--- server/db/mong.js
+++
@@ -1,9 +0,0 @@
-var  mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/ToDoApp'); // , { useMongoClient: true }
-mongoose.Promise = global.Promise; // to be able to access Promise after db operations
-
-
-module.exports = {
-  mongoose
-};
